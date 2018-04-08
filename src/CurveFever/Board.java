7ca35e3b815c17bcd42db08a657886404def04c4
VladStarostenko <38181661+VladStarostenko@users.noreply.github.com>
2018-04-08 16:28:44 +0200

    Update Board.java

--- src/CurveFever/Board.java
+++ src/CurveFever/Board.java
@@ -1,4 +1,18 @@
 package CurveFever;
 
 public class Board {
+    public int[][] boardArray;
+    privat int size;
+    
+    public void Board(int size) {
+    
+    }
+    
+    public int getSize() {
+        return this.size;
+    }   
+    
+    public boolean checkSpace(Point position, int size) {
+        return this.position;
+    }
 }
