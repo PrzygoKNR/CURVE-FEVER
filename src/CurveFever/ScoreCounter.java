c3dd0032bc22539364a5ce5c2e293463b4cba2de
VladStarostenko <38181661+VladStarostenko@users.noreply.github.com>
2018-04-08 16:15:10 +0200

    Update ScoreCounter.java

--- src/CurveFever/ScoreCounter.java
+++ src/CurveFever/ScoreCounter.java
@@ -1,4 +1,13 @@
 package CurveFever;
 
 public class ScoreCounter {
+    private int[] scores;
+
+    public void increseScore(int id, int value) {}
+
+    public int getScore() {
+      return scores;
+    }
+
+    public void clearScore() {}
 }
