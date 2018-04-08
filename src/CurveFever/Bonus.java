4d8cd1e2121a31aa39f00b62d7359210b8e11a34
VladStarostenko <38181661+VladStarostenko@users.noreply.github.com>
2018-04-08 16:46:37 +0200

    Update Bonus.java

--- src/CurveFever/Bonus.java
+++ src/CurveFever/Bonus.java
@@ -2,4 +2,16 @@ package CurveFever;
 
 public class Bonus {
 
+    private static int maxId = 0;
+    private int bonusID;
+    private BonusType type;
+    public Point2D position;
+    
+    public boolean handleBonus(JList<Player> list) {
+        return list;
+    }
+    
+    public void Bonus(int x, int y) {
+        
+    }
 }
