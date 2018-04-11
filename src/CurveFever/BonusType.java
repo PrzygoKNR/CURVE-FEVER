1f917f03b37f02207e82fd6dfb4d3be3d3250f65
KK <you@example.com>
2018-04-11 17:40:10 +0200

    Usunięcie śmieciowych plików

--- src/CurveFever/BonusType.java
+++
@@ -1,5 +0,0 @@
-package CurveFever;
-
-public enum BonusType {
-    undefined
-}
--- src/CurveFever/IDrawable.java
+++
@@ -1,7 +0,0 @@
-package CurveFever;
-
-import java.awt.geom.Point2D;
-
-public interface IDrawable {
-    void Draw(Point2D point, int size);
-}
